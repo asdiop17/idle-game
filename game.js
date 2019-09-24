89318a459a2c15159c3e18cf5ad5f19df12e7d9c
Abou <Asdiop17@harrisonburg.k12.va.us>
2019-09-24 09:51:21 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
-var welcome = new title('Title');
-var woodButton = new Button('Collect Wood') , increaseWood
+var welcome = new Title('The Survivor');
+var woodButton = new Button('Collect Wood', increaseWood);
 var wood = 0;
-var woodcount = new Text(Wood);
+var woodcount = new Text(wood);
 
 
 
@@ -10,4 +10,17 @@ var woodcount = new Text(Wood);
 
 function increaseWood() {
   wood++;
-  woodcount.edit(Wood);
+  woodcount.edit(wood);
+}
+
+var metalButton = new Button('Collect Metal', increaseMetal);
+var metal = 0;
+var metalcount = new Text(wood);
+
+function increaseMetal(){
+  metal++;
+  metalcount.edit(metal)
+}
+
+if (metalcount == 50);
+new Button ('Collect Food');
